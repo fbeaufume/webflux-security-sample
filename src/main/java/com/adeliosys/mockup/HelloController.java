e999dd316d752890087dc1c952f55914bd4f0dc5
Florian Beaufumé <fbeaufume@gmail.com>
2018-09-22 18:02:16 +0200

    Removed the configurable pause

--- src/main/java/com/adeliosys/mockup/HelloController.java
+++ src/main/java/com/adeliosys/mockup/HelloController.java
@@ -3,12 +3,10 @@ package com.adeliosys.mockup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
-import java.time.Duration;
 
 /**
  * REST endpoint for various utility methods.
@@ -19,23 +17,13 @@ public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/public")
-    public Mono<String> publicHello(
-            @RequestParam(required = false, defaultValue = "200") long duration,
-            Principal principal) {
-        return sayHello(principal, "public", duration);
+    public Mono<String> publicHello(Principal principal) {
+        return Mono.just("Hello " + getPrincipalName(principal) + ", from a public page");
     }
 
     @GetMapping("/private")
-    public Mono<String> privateHello(
-            @RequestParam(required = false, defaultValue = "200") long duration,
-            Principal principal) {
-        return sayHello(principal, "private", duration);
-    }
-
-    private Mono<String> sayHello(Principal principal, String pageType, long duration) {
-        return Mono.delay(Duration.ofMillis(duration))
-                .doFinally(s -> LOGGER.info("Paused for {} msec", duration))
-                .then(Mono.just("Hello " + getPrincipalName(principal) + ", from a " + pageType + " page"));
+    public Mono<String> privateHello(Principal principal) {
+        return Mono.just("Hello " + getPrincipalName(principal) + ", from a private page");
     }
 
     private String getPrincipalName(Principal principal) {
