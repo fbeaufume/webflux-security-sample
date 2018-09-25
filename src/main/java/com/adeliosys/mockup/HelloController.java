c134cbcbad11c33fe2d6162ec11a450d72cb74bb
Florian Beaufumé <fbeaufume@gmail.com>
2018-09-25 17:59:48 +0200

    Removed unused logger

--- src/main/java/com/adeliosys/mockup/HelloController.java
+++ src/main/java/com/adeliosys/mockup/HelloController.java
@@ -1,7 +1,5 @@
 package com.adeliosys.mockup;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -13,8 +11,6 @@ import java.security.Principal;
  */
 @RestController
 public class HelloController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/public")
     public Mono<String> publicHello(Principal principal) {
