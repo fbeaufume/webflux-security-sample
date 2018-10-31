34278ca092194e56bda46c498587d9fe82a092fd
Florian Beaufumé <fbeaufume@gmail.com>
2018-10-31 19:45:25 +0100

    Code polish

--- src/main/java/com/adeliosys/sample/ReactiveSecuredApplication.java
+++ src/main/java/com/adeliosys/sample/ReactiveSecuredApplication.java
@@ -3,11 +3,7 @@ package com.adeliosys.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Sample insurance application that provides insurance quotes.
- */
 @SpringBootApplication
-
 public class ReactiveSecuredApplication {
 
     public static void main(String[] args) {
